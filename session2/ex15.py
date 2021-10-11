8e3a2d1d534b73131430a6c9147301ae73fddc3f
Andrei Popovici <andrei_popovici@hotmail.com>
2021-10-12 01:18:51 +0300

    Update ex15.py
    Solution

--- session2/ex15.py
+++ session2/ex15.py
@@ -6,3 +6,12 @@
         Veti primi: 'cmi'
         Veti printa: 1
 """
+x = input("Introduceti sirul de caractere:")
+x_lowercase = x.lower()
+total_vowel_count = 0
+for vowel in 'aeiou':
+    count = x_lowercase.count(vowel)
+    total_vowel_count[vowel] += count
+print(total_vowel_count)
+
+
