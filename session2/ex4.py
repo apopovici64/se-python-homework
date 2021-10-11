cbd5db9bff012e7b4c2ce239fb28ed7e979cdd4e
Andrei Popovici <andrei_popovici@hotmail.com>
2021-10-12 01:19:28 +0300

    Update ex4.py
    Solution

--- session2/ex4.py
+++ session2/ex4.py
@@ -4,14 +4,14 @@
 """
 
 # In variabila t1 vom stoca un tuplu cu 2 elemente, 1 si 2
-# iar in variabila t2 vom stoca un tuplu cu 3 element 3, 4, 5
+# iar in variabila t2 vom stoca un tuplu cu 3 elemente 3, 4, 5
 t1 = (1, 2)
 t2 = (3, 4, 5)
 
 # Vom crea variabila l1 iar ca si valoare, va avea o lista compusa din
 # concatenarea celor 2 tupluri. Primele elemente vor fi cele din l1.
 # Vom converti cele 2 tupluri in liste, inainte sa le concatenam
-l1 = list(t1) + list(t2)
+l1 = list(t1) + [2] + list(t2)
 
 # Afisam lista
 print(l1)
