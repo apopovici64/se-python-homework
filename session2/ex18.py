9f577c40897494c27496d0cf07385a2eff2f9f27
Andrei Popovici <andrei_popovici@hotmail.com>
2021-10-12 02:25:27 +0300

    Update ex18.py
    Solution

--- session2/ex18.py
+++ session2/ex18.py
@@ -7,3 +7,6 @@
         Veti primi: 'Center for Intelligent Machines', 2, 5
         Veti printa: 'nter'
 """
+s = input("Introduceti sirul de caractere:")
+x, y = [int(a) for a in input("Introduceti doua numere intregi:").split()]
+print(s[x:y])
