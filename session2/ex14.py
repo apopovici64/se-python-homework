d8517a0742b7079a28bb81820238eeab9605d81c
Andrei Popovici <andrei_popovici@hotmail.com>
2021-10-12 01:18:44 +0300

    Update ex14.py
    Solution

--- session2/ex14.py
+++ session2/ex14.py
@@ -6,3 +6,7 @@
         Veti primi: 'cmi'
         Veti printa: 3
 """
+
+x = input("Introduceti sirul de caractere:")
+# Printare numar litere ce formeaza sirul
+print(f"Sirul are {len(x)} caractere")
