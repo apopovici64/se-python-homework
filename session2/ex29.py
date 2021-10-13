bb5022cffcdacab0cf244f9408e157dd39bb7967
Andrei Popovici <andrei_popovici@hotmail.com>
2021-10-13 19:43:55 +0300

    Update ex29.py

--- session2/ex29.py
+++ session2/ex29.py
@@ -9,3 +9,14 @@
         2 (pentru vocale)
         4 (pentru consoane)
 """
+x = input("Introduceti sirul de caractere:")
+x_lowercase = x.lower()
+total_vowel_count = 0
+total_consonant_count = 0
+for vowel in 'aeiou': #consideram vocalele din limba engleza
+    count_v = x_lowercase.count(vowel)
+    total_vowel_count += count_v
+ctotal_consonant_count = len(x) - total_vowel_count
+
+print(total_vowel_count)
+print(total_consonant_count)
