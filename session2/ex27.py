532848962fceb6a562ac75f3ef5a8d699cef8200
Andrei Popovici <andrei_popovici@hotmail.com>
2021-10-13 19:43:40 +0300

    Update ex27.py
    Solution

--- session2/ex27.py
+++ session2/ex27.py
@@ -7,3 +7,10 @@
         Veti primi: 5
         Veti printa: 'ashdj' (poate fi orice alt string)
 """
+import string
+import random
+x = input("Introduceti un numar intreg:")
+s = ''
+ran = s.random.choice(string.ascii_lowercase + string.digits, k = x)
+# Rezultat
+print("Stringul aleator este: " + str(ran))
