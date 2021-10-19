5feefe7eb83e2f62757113f6ffd5f2b25dca40ce
Andrei Popovici <andrei_popovici@hotmail.com>
2021-10-19 15:27:08 +0300

    Update ex7.py

--- session3/ex7.py
+++ session3/ex7.py
@@ -12,3 +12,11 @@
     Observatii:
         - functia trebuie sa aiba MAXIM 1 linie de cod ca si body
 """
+def add_pref_str_suff(prefix, word, suffix):
+    return prefix + word + suffix
+
+prf = input('Introduceti un prefix:')
+wrd = input('introduceti  un sir de caractere:')
+suf = input('Introduceti un sufix:')
+
+print(add_pref_str_suff(prf, wrd, suf))
